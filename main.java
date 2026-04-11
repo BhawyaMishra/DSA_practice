0f0af53284fb66759a29c701f39a8e09a61c4496
BhawyaMishra <151458045+BhawyaMishra@users.noreply.github.com>
2026-04-12 02:19:12 +0530

    Add files via upload
    Simple program for average temperature

---
+++ main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class main {
+    public static void main(String[] args) {
+        Scanner console= new Scanner(System.in);
+        System.out.println("How many days' temperatures? ");
+        int numDays= console.nextInt();
+        int sum=0;
+
+        for (int i=1; i<=numDays; i++){
+            System.out.println("Day "+i+" 's high temp: ");
+            int next= console.nextInt();
+            sum+=next;
+        }
+
+        double average= sum/numDays;
+        System.out.println();
+        System.out.println("Average Temp= "+ average);
+    }
+}
