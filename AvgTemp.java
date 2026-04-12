71c32126e7d3bc48d1dc4324d856b2ea9fc7443d
Bhawya Mishra <151458045+BhawyaMishra@users.noreply.github.com>
2026-04-12 16:16:46 +0530

    Update AvgTemp.java

--- AvgTemp.java
+++ AvgTemp.java
@@ -1,19 +1,31 @@
 import java.util.*;
+
 public class main {
     public static void main(String[] args) {
-        Scanner console= new Scanner(System.in);
+        Scanner console = new Scanner(System.in);
         System.out.println("How many days' temperatures? ");
-        int numDays= console.nextInt();
-        int sum=0;
+        int numDays = console.nextInt();
+        int[] temps = new int[numDays];
+        int sum = 0;
 
-        for (int i=1; i<=numDays; i++){
-            System.out.println("Day "+i+" 's high temp: ");
-            int next= console.nextInt();
-            sum+=next;
+        for (int i = 0; i < numDays; i++) {
+            System.out.println("Day " + (i + 1) + "'s high temp: ");
+            temps[i] = console.nextInt();
+            sum += temps[i];
         }
 
-        double average= sum/numDays;
+        double average = (double) sum / numDays;
+
+        int above = 0;
+        for (int i = 0; i < temps.length; i++) {
+            if (temps[i] > average) {
+                above++;
+            }
+        }
+
         System.out.println();
-        System.out.println("Average Temp= "+ average);
+        System.out.println("Average Temp = " + average);
+        System.out.println(above + " days above average");
+        console.close();
     }
 }
